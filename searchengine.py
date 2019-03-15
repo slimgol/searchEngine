6b30eb695969f9fe58347b3a961bfa61b27d4612
slimgol <slimgol@users.noreply.github.com>
2019-03-15 14:08:39 -0700

    new file added

--- searchengine.py
+++ searchengine.py
@@ -1,3 +1,11 @@
+import urllib2
+from BeautifulSoup import *
+from urlparse import urljoin
+
+#Create list of words to ignore.
+ignoreWords = set(['the', 'of', 'to', 'and', 'a', 'in', 'is', 'it'])
+
+
 class crawler:
     #Initialize the crawler with the name of the database.
     def __init__(self, dbname):
@@ -35,7 +43,30 @@ class crawler:
 
     #Starting with a list of pages, do a breadth first search to the given depth, indexing pages as we go.
     def crawl(self, pages, depth=2):
-        pass
+        for i in range(depth):
+            newpages = set()
+            for page in pages:
+                try:
+                    c = urllib2.urlopen(page)
+                except:
+                    print("Could not open %s" % page)
+                    continue
+                soup = BeautifulSoup(c.read())
+                self.addtoindex(page, soup)
+
+                links = soup('a')
+                for link in links:
+                    if ('href' in dict(link.attrs)):
+                        url = urljoin(page, link['href'])
+                        if url.find("'")!=-1: continue
+                        url = url.split('#')[0]#Remove location portion.
+                        if url[0:4] == 'http' and not self.isindexed(url):
+                            newpages.add(url)#Add URL.
+                        linkText = self.gettextonly(link)#Extract the text from the web resource.
+                        self.addlinkref(page, url, linkText)
+
+                        self.dbcommit()#Save the changes made to the database.
+                pages = newpages
 
     #Create the database tables.
     def createindextables(self):
