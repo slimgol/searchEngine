9b66984f876d002c49da41bc1a6a9267882056a3
kyro1010 <kyrojgibling@gmail.com>
2019-03-23 22:52:11 -0400

    Changes made

--- test.py
+++ test.py
@@ -1,9 +1,12 @@
 import nltk
 from nltk.corpus import state_union
 from nltk.tokenize import PunktSentenceTokenizer
+from nltk.stem import WordNetLemmatizer
 
-train_text = state_union.raw("2005-GWBush.txt")
-sample_text = state_union.raw("2006-GWBush.txt")
+'''Note that the PunktSentenceTokenizer uses unsupervised learning, and thus we only
+need some input text to train it. We will use the George Washington State Union Speech 
+from 2002.'''
+train_text = state_union.raw("2002-GWBush.txt")#Training text.
 
 custom_sent_tokenizer = PunktSentenceTokenizer(train_text)
 
@@ -13,5 +16,18 @@ print(tokenized)
 words = nltk.word_tokenize(tokenized[0])
 print(words)
 tagged = nltk.pos_tag(words)
-print(tagged)
+print(tagged[0][1].startswith('N'))
 
+def convert_to_wn(tagged_text):
+	for t in tagged_text:
+		
+
+
+lemmatizer = WordNetLemmatizer()#Instantiate WordNetLemmatizer class.
+'''
+for t in tagged:
+	try:
+		print(lemmatizer.lemmatize(t[0], t[1]))
+	except Exception as e:
+		pass
+'''
