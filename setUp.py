83639e55eb8e068dfa743398837909b6369f9fb1
kyro1010 <kyrojgibling@gmail.com>
2019-04-02 14:50:35 -0400

    Changes made

--- setUp.py
+++ setUp.py
@@ -14,4 +14,84 @@ import trainTestData #Import function to extract text from a given url, and norm
 
 normalized_labeled_set = trainTestData.createTaggedDataSet(seedPages.ENERGY_SEED_URLS)
 
-print(normalized_labeled_set[0])
+print(normalized_labeled_set[3])
+print(normalized_labeled_set[2])
+
+
+'''
+TODO: Prepare training and testing data sets. Thus, split the normalized labaled set into 
+a normalized training set and a normalized testing set.
+'''
+
+#TODO: Create function here.
+
+#TODO: Form the following:
+#TODO: From a single normalized labeled corpus, namely: normalized_labeled_set, create the following subsets.
+#normalized_labeled_train, normalized_labeled_test --> derived from normalized_labeled_set
+
+#Create the train corpus, and their associated labels.
+normalized_train_corpus = []
+train_labels = []
+for pair in normalized_labeled_train:
+	normalized_train_corpus.append(pair[0])#Text.
+	train_labels.append(pair[1])#Label
+
+#Create the test corpus, and their associated labels.
+normalized_test_corpus = []
+test_labels = []
+for pair in normalized_labeled_test:
+	normalized_test_corpus.append(pair[0])#Text
+	test_labels.append(pair[1])#Label
+
+
+
+
+from featureExtraction import bow_extractor
+import nltk
+
+#Bag-of-words features.
+#Train bow vectorizer on the normalized train corpus.
+bow_vectorizer, bow_train_features = bow_extractor(normalized_train_corpus)
+#Use the trained vectorizer to transform the normalized test corpus, to create the bag of words for the normalized test corpus.
+bow_test_features = bow_vectorizer.transform(normalized_test_corpus)
+
+
+'''
+TODO: Explain what we are doing here.
+Approach:
+
+'''
+
+def train_predict_evaluate_model(classifier, train_features, train_labels, test_features, test_labels):
+	#Build model.
+	classifier.fit(train_features,train_labels)
+	#Predict using model.
+	predictions = classifier.predict(test_features)
+	#Evaluate model prediction performance.
+	get_metrics(true_labels=test_labels,predicted_labels=predictions)
+	return predictions
+
+from sklearn.naive_bayes import MultinomialNB
+
+mnb = MultinomialNB()
+
+
+'''
+Now we will train, predict, and evaluate models for all the different types of features using
+Multinomial Naive Bayes and Support Vector Machines.
+'''
+
+#Multinomial Naive Bayes with BOW features.
+mnb_bow_predictions = train_predict_evaluate_model(classifier=mnb, train_features=bow_train_features,train_labels=train_labels,test_features=bow_test_features,test_labels=test_labels)
+
+
+
+
+
+
+
+
+
+
+
+
