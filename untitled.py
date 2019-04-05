bc6dcb066004ff2dcdd0c4167d05c0fa99c56f31
kyro1010 <kyrojgibling@gmail.com>
2019-04-05 00:45:41 -0400

    Changes made

--- untitled.py
+++ untitled.py
@@ -21,55 +21,13 @@ Approach:
 
 import seedPages#Seed pages that will be used to get the training and test data for the classifier.
 import trainTestData #Import function to extract text from a given url, and normalize it, returning a list of pairs of the form (url, topic).
-
-normalized_labeled_set = trainTestData.createTaggedDataSet(seedPages.ENERGY_SEED_URLS)
-
-
-
-
-'''
-TODO: Prepare training and testing data sets. Thus, split the normalized labaled set into 
-a normalized training set and a normalized testing set.
-'''
-
-#TODO: Split normalized_labeled_set into training and test sets.
-#normalized_labeled_train, normalized_labeled_test --> derived from normalized_labeled_set
-
-#Create the train corpus, and their associated labels.
-normalized_train_corpus = []
-train_labels = []
-for pair in normalized_labeled_train:
-	normalized_train_corpus.append(pair[0])#Text.
-	train_labels.append(pair[1])#Label
-
-#Create the test corpus, and their associated labels.
-normalized_test_corpus = []
-test_labels = []
-for pair in normalized_labeled_test:
-	normalized_test_corpus.append(pair[0])#Text
-	test_labels.append(pair[1])#Label
-
-
-
-############################
-####FEATURE EXTRACTION######     
-############################
-
 from featureExtraction import bow_extractor
 import nltk
-
-#Bag-of-words features.
-#Train bow vectorizer on the normalized train corpus.
-bow_vectorizer, bow_train_features = bow_extractor(normalized_train_corpus)
-#Use the trained vectorizer to transform the normalized test corpus, to create the bag of words for the normalized test corpus.
-bow_test_features = bow_vectorizer.transform(normalized_test_corpus)
+from trainTestData import extractText#Used to extract text from a given url.
+from test import normalize_text#Used to normalize the extracted text.
+from sklearn.naive_bayes import MultinomialNB
 
 
-'''
-TODO: Explain what we are doing here.
-Approach:
-
-'''
 
 def train_predict_evaluate_model(classifier, train_features, train_labels, test_features, test_labels):
 	#Build model.
@@ -80,24 +38,54 @@ def train_predict_evaluate_model(classifier, train_features, train_labels, test_
 	get_metrics(true_labels=test_labels,predicted_labels=predictions)
 	return predictions
 
-from sklearn.naive_bayes import MultinomialNB
 
-mnb = MultinomialNB()
-
-
-'''
-Now we will train, predict, and evaluate models for all the different types of features using
-Multinomial Naive Bayes and Support Vector Machines.
-'''
 
 #Multinomial Naive Bayes with BOW features.
 mnb_bow_predictions = train_predict_evaluate_model(classifier=mnb, train_features=bow_train_features,train_labels=train_labels,test_features=bow_test_features,test_labels=test_labels)
 
-#Build model.
-classifier.fit(bow_train_features,train_labels)
 
-from trainTestData import extractText#Used to extract text from a given url.
-from test import normalize_text#Used to normalize the extracted text.
+#Create a function to train the model.
+#This function must check to determine whether or not the classifier has been trained.
+#If not trained then train.
+#Otherwise, load model from storage.
+def trainModel(classifier):
+	#TODO: Test to determine whether or not the model has been trained and stored.
+
+	#Form a normalized tagged dataset from a set of predefined, labeled urls.
+	normalized_labeled_set = trainTestData.createTaggedDataSet(seedPages.ENERGY_SEED_URLS)
+	
+	#TODO: Split normalized_labeled_set into training and test sets.
+	#normalized_labeled_train, normalized_labeled_test --> derived from normalized_labeled_set
+
+	#Create the train corpus, and their associated labels.
+	normalized_train_corpus = []
+	train_labels = []
+	for pair in normalized_labeled_train:
+		normalized_train_corpus.append(pair[0])#Text.
+		train_labels.append(pair[1])#Label
+
+	#Create the test corpus, and their associated labels.
+	normalized_test_corpus = []
+	test_labels = []
+	for pair in normalized_labeled_test:
+		normalized_test_corpus.append(pair[0])#Text
+		test_labels.append(pair[1])#Label
+
+	#Bag-of-words features.
+	#Train bow vectorizer on the normalized train corpus.
+	bow_vectorizer, bow_train_features = bow_extractor(normalized_train_corpus)
+	#Use the trained vectorizer to transform the normalized test corpus, to create the bag of words for the normalized test corpus.
+	bow_test_features = bow_vectorizer.transform(normalized_test_corpus)
+
+	#Build model using the bow training features, and the corresponding labels.
+	classifier.fit(bow_train_features, train_labels)
+
+
+
+
+
+#TODO: Instantiate classifier here.
+mnb = MultinomialNB()
 
 #TODO: Explain what this function does.
 def makePrediction(url):
@@ -108,6 +96,12 @@ def makePrediction(url):
 	normalizedText = normalize_text(text)
 	#Create bag of words features from the normalized text. We will use our pre-trained bow vectorizer.
 	bow_features = bow_vectorizer.transform([normalizedText])#Note that "transform" method accepts a list of strings.
+	#Make prediction.
+	prediction = classifier.predict(bow_features)
+	return prediction #TODO: Explain what data type is returned, and also whatelese needs to be done.
+	
+
+
 
 
 
