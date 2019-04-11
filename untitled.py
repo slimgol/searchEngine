cca18c9318b5c814f516fc8653f8e6793c9448f6
kyro1010 <kyrojgibling@gmail.com>
2019-04-10 23:02:15 -0400

    Changes made

--- untitled.py
+++ untitled.py
@@ -21,14 +21,79 @@ Approach:
 
 import seedPages#Seed pages that will be used to get the training and test data for the classifier.
 import trainTestData #Import function to extract text from a given url, and normalize it, returning a list of pairs of the form (url, topic).
-from featureExtraction import bow_extractor
 import nltk
 from trainTestData import extractText#Used to extract text from a given url.
 from test import normalize_text#Used to normalize the extracted text.
 from sklearn.naive_bayes import MultinomialNB
 import random#Used for generating pseudorandom numbers.
+from sklearn.feature_extraction.text import CountVectorizer
+from bs4 import BeautifulSoup
+from urllib import request
+from test import normalize_text#Import function to normalize a string of text.
 
 
+
+'''
+This function will accept a list of strings and train a bag-of-words (bow) feature extractor.
+This function returns the trained bow feature extractor, as well as the corresponding features that
+were produced from the list of strings. Thus, it will return a list of lists (engineered features).
+'''
+def bow_extractor(corpus, ngram_range=(1,1)):
+	vectorizer = CountVectorizer(min_df=1,ngram_range=ngram_range)#Accept terms with a minimum frequency of 1.
+	features = vectorizer.fit_transform(corpus)
+
+	return vectorizer, features
+
+
+
+
+'''
+Function accepts a url, extracts text from the url, and then returns the extracted
+text as a single string.
+'''
+
+def extractText(urlString):
+	try:
+		page = request.urlopen(urlString)
+	except:
+		print("Could not open page")
+		return None
+
+	soup = BeautifulSoup(page.read())
+
+	'''Extract all meaningful data. Note that we want any data that will be useful
+	in training a text classigier. Thus, paragraphs, headers, etc...'''
+
+	paragraphList = soup.find_all('p')
+	paragraphString = ""#Create an empty string.
+	for paragraph in paragraphList:#Iterate over all paragraphs.
+		paragraphString = paragraphString + " " + paragraph.get_text()
+
+	return paragraphString
+
+
+
+
+
+'''This accepts a list of pairs--> (url, topic),
+and returns a list of pairs--> (text as single string from url, topic) 
+Note that this function also normalizes the string.
+Returns a list of pairs-->(normalized string, topic).'''
+
+def createTaggedDataSet(urlTaggedList):
+	taggedDataList = []#(text as single string from url, topic)
+	for pair in urlTaggedList:
+		text = extractText(pair[0])#Extract text from url.
+		if (text != None):
+			taggedDataList.append((normalize_text(text), pair[1]))#Add pair (text, topic) to list.
+
+	return taggedDataList
+
+
+
+
+'''
+#TODO: This function needs some revision. Is it even necessary?
 def train_predict_evaluate_model(classifier, train_features, train_labels, test_features, test_labels):
 	#Build model.
 	classifier.fit(train_features,train_labels)
@@ -42,9 +107,18 @@ def train_predict_evaluate_model(classifier, train_features, train_labels, test_
 
 #Multinomial Naive Bayes with BOW features.
 mnb_bow_predictions = train_predict_evaluate_model(classifier=mnb, train_features=bow_train_features,train_labels=train_labels,test_features=bow_test_features,test_labels=test_labels)
+'''
 
 
-#TODO: Explain what this function does.
+
+'''
+This function accepts a list of pairs of the form, (url, topic), as well as a training proportion.
+The training proportion is used to indicate how much of the input training data is used for 
+training and how much is used for testing. This function then returns two lists. The first list
+contains the training pairs, and the second list contains the testing pairs; both of the form
+(url, topic).
+'''
+
 def trainTestSplit(inputList, train_proportion=0.8):
 	if (training_proportion > 1 || training_proportion < 0):
 		return None
@@ -73,10 +147,15 @@ def trainTestSplit(inputList, train_proportion=0.8):
 	#Return the training and testing lists.
 	return training_list, testing_list
 
-#Create a function to train the model.
-#This function must check to determine whether or not the classifier has been trained.
-#If not trained then train.
-#Otherwise, load model from storage.
+
+
+'''
+This function does not accept any arguments. This function checks to determine if a model has been 
+trained and stored on the computer disk, if so, the function will load that model from the disk 
+and return it to the calling function, otherwise it will instantiate the model, train it, and then
+return it to the calling function.
+'''
+
 def trainModel():
 	#TODO: Test to determine whether or not the model has been trained and stored.
 	#If model is trained already, then load it from storage and return it to the calling function.
@@ -86,7 +165,7 @@ def trainModel():
 
 	#TODO: The following name is deceiving as it is a list and not a set; change it.
 	#Form a normalized tagged dataset from a set of predefined, labeled urls.
-	normalized_labeled_set = trainTestData.createTaggedDataSet(seedPages.ENERGY_SEED_URLS)
+	normalized_labeled_set = createTaggedDataSet(seedPages.ENERGY_SEED_URLS)
 	
 	#TODO: Split normalized_labeled_set into training and test sets.
 	#normalized_labeled_train, normalized_labeled_test --> derived from normalized_labeled_set
@@ -123,7 +202,13 @@ def trainModel():
 classifier = trainModel()
 
 
-#TODO: Explain what this function does.
+
+'''
+This function accepts a raw url, reads the content of the web-page from the url, normalizes the
+text, and uses the trained classifier to make a prediction.
+This function then returns the prediction to the calling function.
+'''
+
 def classifyUrl(url):
 	text = extractText(url)
 	if (text == None):
